3aebb03b7a2527024ad878803b049772e1220e52
minseo <136690999+kingkaminseo@users.noreply.github.com>
2026-05-27 16:15:16 +0900

    Time: 491 ms (45.78%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    id, 
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN(
+                    (
+                        SELECT
+                             P_id 
+                        FROM Tree
+                    )
+                ) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree;
