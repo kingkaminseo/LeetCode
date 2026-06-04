b55cca00a52dbab8ac738dbf9eef939390e5c8b5
minseo <136690999+kingkaminseo@users.noreply.github.com>
2026-06-04 11:51:43 +0900

    Time: 105 ms (28.57%), Space: 64.5 MB (35.71%) - LeetHub

---
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.ts
@@ -0,0 +1,17 @@
+function totalWaviness(num1: number, num2: number): number {
+    let count = 0;
+    for (let i = num1; i <= num2; i++) {
+
+        const waveArr = i.toString().split('').map(Number);
+        for (let j = 0; j < waveArr.length; j++) {
+            if (j === 0 || j === waveArr.length) {
+                continue;
+            }
+            if (waveArr[j -1] < waveArr[j] && waveArr[j] > waveArr[j + 1]
+            || waveArr[j -1] > waveArr[j] && waveArr[j] < waveArr[j + 1]) {
+                count = count + 1
+            }
+        };
+    };
+    return count;
+};
